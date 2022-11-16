b49ca7f5791e829649988da8abf5ccf70157945b
KirillZhel <Zheltyshev.kirill@gmail.com>
2022-11-17 01:19:20 +0300

    Add project files.

---
+++ CreationalPatterns/FactoryMethod.cs
@@ -0,0 +1,65 @@
+﻿namespace CreationalPatterns
+{
+    public interface ITransport
+    {
+        public Guid TransportId { get; set; }
+        public string Deliver(string start, string end);
+    }
+
+    public class Car : ITransport
+    {
+        public Guid TransportId { get; set; }
+
+        public Car()
+        {
+            TransportId = Guid.NewGuid();
+        }
+
+        public string Deliver(string start, string end)
+        {
+            return $"{nameof(Car)} with ID \"{TransportId}\" start: {start}, end: {end}";
+        }
+    }
+
+    public class Ship : ITransport
+    {
+        public Guid TransportId { get; set; }
+
+        public Ship()
+        {
+            TransportId = Guid.NewGuid();
+        }
+
+        public string Deliver(string start, string end)
+        {
+            return $"{nameof(Ship)} with ID \"{TransportId}\" start: {start}, end: {end}";
+        }
+    }
+
+    public abstract class TransportLogistics
+    {
+        public void PlanDelivery(string start, string end)
+        {
+            ITransport t = CreateTransport();
+            Console.WriteLine(t.Deliver(start, end));
+        }
+
+        public abstract ITransport CreateTransport();
+    }
+
+    public class CarLogistics : TransportLogistics
+    {
+        public override ITransport CreateTransport()
+        {
+            return new Car();
+        }
+    }
+
+    public class ShipLogistics : TransportLogistics
+    {
+        public override ITransport CreateTransport()
+        {
+            return new Ship();
+        }
+    }
+}
---
+++ CreationalPatterns/Program.cs
@@ -0,0 +1,39 @@
+﻿using CreationalPatterns;
+
+UseFactoryMethod();
+
+
+/*
+ * ПРИМЕНЯЕМОСТЬ:
+ * 
+ * Когда заранее неизвестны типы и зависимости объектов,
+ * с которыми должен работать ваш код.
+ * 
+ * Когда вы хотите дать возможность пользователям расширять части вашего фреймворка или библиотеки.
+ * 
+ * Когда вы хотите экономить системные ресурсы,
+ * повторно используя уже созданные объекты,
+ * вместо создания новых.
+ */
+void UseFactoryMethod()
+{
+    TransportLogistics transportLogistics;
+
+    var typeTransport = Console.ReadLine().ToLower();
+
+    switch (typeTransport)
+    {
+        case "ship":
+            transportLogistics = new ShipLogistics();
+            break;
+        case "car":
+            transportLogistics = new ShipLogistics();
+            break;
+        default:
+            Console.WriteLine("вы не выбрали тип транспорта. Доставим машиной.");
+            transportLogistics = new ShipLogistics();
+            break;
+    }
+
+    transportLogistics.PlanDelivery("Moscow", "Petersburg");
+}
