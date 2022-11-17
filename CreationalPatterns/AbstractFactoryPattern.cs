773ddffc84cf8238091dc196984c1c3de86a9d07
Кирилл Желтышев <zheltyshev.kirill@gmail.com>
2022-11-18 02:48:37 +0300

    реализовал три интерфейса для примера абстрактной фабрики

--- CreationalPatterns/AbstractFactoryPattern.cs
+++ CreationalPatterns/AbstractFactoryPattern.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CreationalPatterns
 {
-    public class AbstractFactoryPattern
-    {
-    }
+	public interface IChair
+	{
+		public bool HasLegs { get; set; }
+		
+		public bool SitOn();
+	}
+
+	public interface ITable
+	{
+		public void PutOn(string thing);
+	}
+
+	public interface IFurnitureFactory
+	{
+		public IChair CreateChair();
+		public ITable CreateCoffeTable();
+	}
 }
